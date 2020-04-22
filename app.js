7a91b1af321b453fda9173b479ea173a5afaa3eb
connisimmons <connisimmons@gmail.com>
2020-04-22 14:41:42 -0600

    first

---
+++ app.js
@@ -0,0 +1,57 @@
+console.log( "Simple Web Server Assignment")
+
+const express = require('express');
+const port = 3036;
+const appName = 'Simple Express Web Server Assignment';
+const app = express();
+const server = express();
+
+
+// Middleware
+app.use(middle); 
+function middle(req, res, next) {
+	console.log('Server has received a request. . . ');
+	next();
+	};
+
+app.get('/test', (req, res, next) => {
+    res.send('It is working');        
+})  
+
+app.use(express.static(`public`));
+
+app.get('/other', (req, res, next) => {
+        res.set({
+        "Content-Type": "text/html"
+    });
+    const body = `
+        <h1>Other Route!</h1>
+        <p>You have requested an HTML response</p>
+    `;
+    res.send(body);
+});
+
+const logger = (req, res, next) => {
+    console.log(req.method, req.path);
+    next();
+}
+app.use(logger);
+app.use(express.json());
+
+
+app.get('/', (req, res, next) => {
+    res.send('You made it.');
+}); 
+
+
+
+
+
+
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log('Error launching app: ', err);
+    }
+    console.log(`${appName} listening on port ${port}...`);
+});
